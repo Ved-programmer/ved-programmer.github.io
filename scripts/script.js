4adfaedc4b9d6962b2c10002aad0f16c703b5754
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-05-25 21:35:49 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -40,36 +40,6 @@ function typeWriter(){
   setTimeout(typeWriterBeginning, delay*2 + timeToExectueFirstTypeWriting, 0, lowerText, id2, speed);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-      
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop + 150 < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-              }
-          });
-          if(lazyloadImages.length == 0) { 
-            document.removeEventListener("scroll", lazyload);
-            window.removeEventListener("resize", lazyload);
-            window.removeEventListener("orientationChange", lazyload);
-          }
-      }, 0);
-    }
-    
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  });
-
 var typed = new Typed('#aboutMeSwitch', {
     strings: ["a Blogger", "a Pro Programmer", "a Pythonista", "an AlgoExpert"],
     typeSpeed: 50,
